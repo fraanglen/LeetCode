3e1856cd2b42772195c5d9f6eb331b6faf195fe8
fraanglen <fraanglen1225@gmail.com>
2025-09-05 07:54:39 +0530

    Added eount Element Max freq file

---
+++ String/countElementWithMaxFreq.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer,Integer>freq=new HashMap<>();
+        for(int num:nums){
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        }
+        int maxFreq=0;
+        for(int f:freq.values()){
+            maxFreq=Math.max(maxFreq,f);
+        }
+        int total=0;
+        for(int f:freq.values()){
+            if(f==maxFreq){
+                total+=f;
+            }
+        }
+        return total;
+    }
+}
