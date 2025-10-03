79361eb36691b6891893c422d49a8738f32b8bf5
fraanglen <fraanglen1225@gmail.com>
2025-10-03 22:07:43 +0530

    Added 1436 file

---
+++ String/DestinationCity.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+     Set<String>set=new HashSet<>();
+     for(List<String> path:paths){
+        set.add(path.get(0));
+     }   
+    for(List<String>path:paths){
+        String city=path.get(1);
+        if(!set.contains(city)){
+            return city;
+        }
+    }
+    return " ";
+    }
+}
