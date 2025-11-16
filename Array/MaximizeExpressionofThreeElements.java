edf6782711861b75c79cd6e2435cdd2f3d9452f9
fraanglen <fraanglen1225@gmail.com>
2025-11-16 22:04:51 +0530

    Added 3745 file

---
+++ Array/MaximizeExpressionofThreeElements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int num:nums){
+            if(num>max1){
+                max2=max1;
+                max1=num;
+            }
+            else if(num>max2){
+                max2=num;
+            }
+            if(num<min){
+                min=num;
+            }
+        }
+        return max1+max2-min;
+    }
+}
