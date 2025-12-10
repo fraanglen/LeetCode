a4589e8ab5d4b43f1386dfa015247ead883b89ec
fraanglen <fraanglen1225@gmail.com>
2025-12-10 10:35:34 +0530

    Added 1523 file

---
+++ Number Manipulation/Solution.java
@@ -0,0 +1,22 @@
+
+
+public /*class Solution {
+   public int countOdds(int low, int high) {
+        int count=0;
+        for(int i=low;i<=high;i++){
+            if(i%2==1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+*/
+class Solution {
+    public int countOdds(int low, int high) {
+        return (high + 1) / 2 - (low / 2);
+    }
+}
+ {
+    
+}
