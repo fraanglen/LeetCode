8fa10afb90a450cf5455ab95e6a4443d300ce393
fraanglen <fraanglen1225@gmail.com>
2025-12-10 10:37:02 +0530

    Added 3770 file

---
+++ Number Manipulation/LargestPrimefromConsecutivePrimeSum.java
@@ -0,0 +1,26 @@
+
+
+public class Solution {
+    public int largestPrime(int n) {
+        int sum = 0;
+        int ans = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (sum > n) break;
+                if (isPrime(sum)) ans = sum;
+            }
+        }
+        return ans; 
+    }
+        private boolean isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+}
+ {
+    
+}
