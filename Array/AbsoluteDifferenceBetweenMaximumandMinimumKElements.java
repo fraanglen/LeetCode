d882a801ef092dd3a90ae6e89bcf50c6a8490b55
fraanglen <fraanglen1225@gmail.com>
2025-12-28 15:58:00 +0530

    Added 2149 file

--- Array/AbsoluteDifferenceBetweenMaximumandMinimumKElements.java
+++ Array/AbsoluteDifferenceBetweenMaximumandMinimumKElements.java
@@ -1,16 +1,3 @@
-class Solution {
-    public int absDifference(int[] nums, int k) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        int sSum=0;
-        int lSum=0;
-        for(int i=0;i<k;i++){
-            sSum+=nums[i];
-        }
-        for(int i=n-k;i<n;i++){
-            lSum+=nums[i];
-        }
-        int total=Math.abs(sSum-lSum);
-        return total;
-    }
-}©leetcode
+public class AbsoluteDifferenceBetweenMaximumandMinimumKElements {
+    
+}
