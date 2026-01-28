94816b2355b0a8f8d09b9577bde73b3e0df32e56
fraanglen <fraanglen1225@gmail.com>
2026-01-28 10:34:21 +0530

    Added 686 file

---
+++ Linked List/RepeatedStringMatch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int count=1;
+        StringBuilder sb=new StringBuilder(a);
+        while(sb.length()<b.length()){
+            sb.append(a);
+            count++;
+        }
+           if(sb.toString().contains(b)){
+            return count;
+           }
+           sb.append(a);
+           count++;
+           if(sb.toString().contains(b)){
+            return count;
+           }
+        return -1;
+    }
+}
