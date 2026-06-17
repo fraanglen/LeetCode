23cdd54a1ded4c06abceaee0aefdd4599f4b0306
fraanglen <fraanglen1225@gmail.com>
2026-06-17 14:49:44 +0530

    2575 file

---
+++ String/Find the Divisibility Array of a String.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] divisibilityArray(String word, int m) {
+        int n=word.length();
+        int[] arr=new int[n];
+        long rem=0;
+        for(int i=0;i<n;i++){
+            int dig=word.charAt(i)-'0';
+            rem=(rem*10+dig)%m;
+            if(rem==0){
+              arr[i]=1;  
+            }else{
+                arr[i]=0;
+            }
+        }
+        return arr;
+    }
+}
