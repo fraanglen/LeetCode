870fe5b4ea4b950e0129abe7229ffe43f07e6058
fraanglen <fraanglen1225@gmail.com>
2026-06-18 09:45:17 +0530

    198

---
+++ Array/HouseRobber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] arr=new int[n];
+        if(n==1) return nums[0];
+        arr[0]=nums[0];
+        arr[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            arr[i]=Math.max(nums[i]+arr[i-2],arr[i-1]);
+        }
+        return arr[n-1];
+    }
+}
