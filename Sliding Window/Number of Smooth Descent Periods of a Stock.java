bd439121554558fec824d4d1f59ebdbd867c07e2
fraanglen <fraanglen1225@gmail.com>
2026-07-09 11:47:42 +0530

    2110

---
+++ Sliding Window/Number of Smooth Descent Periods of a Stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long count=1;
+        long res=1;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]==prices[i-1]-1){
+                count++;
+            }else{
+                count=1;
+            }
+            res+=count;
+        }
+       return res;
+    }
+}
